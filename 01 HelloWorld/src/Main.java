f854892400c45d21139a9e5a099f1c493aa605a3
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-19 17:56:09 +0530

    learning print method

--- 01 HelloWorld/src/Main.java
+++ 01 HelloWorld/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String args[]) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello, World!"); // This line prints "Hello, World!" to the console
+        System.out.println("Welcome to Java programming!"); // This line prints a welcome message to the console
+
     }
 }
