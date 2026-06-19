9885b8ed7fad8a954b12e402ed21d79fbe01a250
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-20 00:55:08 +0530

    assignmentoperators

---
+++ 01 HelloWorld/src/assignmentoperators.java
@@ -0,0 +1,22 @@
+public class assignmentoperators {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        x += 5; // x is now 15
+        System.out.println(x); // Output: 15
+
+        x -= 3; // x is now 12
+        System.out.println(x); // Output: 12
+
+        x *= 2; // x is now 24
+        System.out.println(x); // Output: 24
+
+        x /= 4; // x is now 6
+        System.out.println(x); // Output: 6
+
+        x %= 5; // x is now 1
+        System.out.println(x); // Output: 1
+
+    }
+}
