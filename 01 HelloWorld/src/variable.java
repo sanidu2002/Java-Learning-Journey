391ff39ad9e4ad8a8c8169211bee1f96195da956
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-19 18:15:03 +0530

    Declare a variable to store a name

---
+++ 01 HelloWorld/src/variable.java
@@ -0,0 +1,28 @@
+public class variable {
+    public static void main(String[] args) {
+        // Declare a variable to store a name
+        int age = 25; // This is an integer variable
+        byte b = 100; // This is a byte variable
+        short s = 10000; // This is a short variable
+        long l = 100000L; // This is a long variable
+        float f = 10.5f; // This is a float variable
+        double d = 20.99; // This is a double variable
+        float f1 = 10.5f; // This is a float variable
+        char c = 'A'; // This is a char variable
+        boolean bool = true; // This is a boolean variable
+        String name = "Sanidu Harsha"; // This is a String variable
+
+        // Print the value of the variable
+        System.out.println(age); // This will print the value of the age variable
+        System.out.println(b); // This will print the value of the byte variable
+        System.out.println(s); // This will print the value of the short variable
+        System.out.println(l); // This will print the value of the long variable
+        System.out.println(f); // This will print the value of the float variable
+        System.out.println(d); // This will print the value of the double variable
+        System.out.println(f1); // This will print the value of the second float variable
+        System.out.println(c); // This will print the value of the char variable
+        System.out.println(bool); // This will print the value of the boolean variable
+        System.out.println(name); // This will print the value of the String variable
+    }
+
+}
