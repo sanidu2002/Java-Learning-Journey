2d447cd52ce1e3ae2ae8d3cd7a545932f0763ff5
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-20 00:47:31 +0530

    add java math operators 2

---
+++ 01 HelloWorld/src/javamathoperators.java
@@ -0,0 +1,28 @@
+public class javamathoperators {
+
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        // Addition
+        int sum = x + y;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = x - y;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = x * y;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = x / y;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus
+        int remainder = x % y;
+        System.out.println("Remainder: " + remainder);
+    }
+}
