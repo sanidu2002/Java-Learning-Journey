33de3c525062aac026be9843d9667f9b1f9f7566
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-20 00:47:12 +0530

    add math operators

---
+++ 01 HelloWorld/src/math.java
@@ -0,0 +1,15 @@
+public class math {
+    public static void main(String[] args){
+
+        double x = 21;
+        double y = 5;
+
+        System.out.println(x+y); // Addition
+        System.out.println(x-y); // Subtraction
+        System.out.println(x*y); // Multiplication
+        System.out.println(x/y); // Division
+        System.out.println(x%y); // Modulus
+
+    }
+
+}
