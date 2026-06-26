011147d6454010d9fff8418073dfa34e354f9f52
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-26 13:31:27 +0530

    String special methods

--- 01 HelloWorld/src/stringmethods.java
+++ 01 HelloWorld/src/stringmethods.java
@@ -1,6 +1,6 @@
 public class stringmethods {
     public static void main(String[] args) {
-        String str = "Hello World";
+        String str = "Hello World";  //string literal
 
         System.out.println("Original : " + str);
 
