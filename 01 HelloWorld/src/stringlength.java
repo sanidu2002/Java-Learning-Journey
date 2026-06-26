de4ecf1abe866021dfdd8e1a362cee1cd0a6462d
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-26 11:32:41 +0530

    String special

---
+++ 01 HelloWorld/src/stringlength.java
@@ -0,0 +1,7 @@
+public class stringlength {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        int length = str.length();
+        System.out.println(length);
+    }
+}
