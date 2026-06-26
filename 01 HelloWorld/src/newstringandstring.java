d350b0a855deb916b5f6d2715dcd3d736f67c1d0
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-26 11:28:37 +0530

    String special

---
+++ 01 HelloWorld/src/newstringandstring.java
@@ -0,0 +1,11 @@
+public class newstringandstring {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String name = new String("John");
+        String str3 = str1 + " " + str2;
+        System.out.println(str3);
+        System.out.println(name);
+    }
+
+}
